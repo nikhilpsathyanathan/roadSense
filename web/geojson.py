4441f5639c9482328c28b86cbc3e60719cda4c9e
Nikhil ps <32610794+nikhilpsathyanathan@users.noreply.github.com>
2019-03-16 10:43:35 +0530

    web

---
+++ web/geojson.py
@@ -0,0 +1,12 @@
+import pandas as pd
+x = pd.read_csv("C:\codepotholes.csv",usecols=['Lat','Lon'])
+
+def df_to_geojson(df, properties, lat='latitude', lon='longitude'):
+    geojson = {'type':'FeatureCollection', 'features':[]}
+    for _, row in df.iterrows():
+        feature = {'type':'Feature','properties':{},'geometry':{'type':'Point','coordinates':[]}}
+        feature['geometry']['coordinates'] = [row[lon],row[lat]]
+        for prop in properties:
+            feature['properties'][prop] = row[prop]
+        geojson['features'].append(feature)
+    return geojson
